42df35731201debcf999460c20ef0d2e95fa52a6
pwickham77 <pwickham@elmontgomery.com>
2024-10-16 11:10:40 -0700

    Update flow processing script and data
    add older USGS gauge data
    Add plots and processing for calibration using Log normalized data

--- discharge/ExtractSimulatedDischarge/PlotSpecifiedReachFlows.py
+++ discharge/ExtractSimulatedDischarge/PlotSpecifiedReachFlows.py
@@ -27,7 +27,8 @@ Created on Thu Oct  3 15:25:13 2024
         
     DEVELOPMENT NOTES
         `Currently set up to extract and process information, and plot htmls and jpgs.
-        ` Still gathering earlier observation data for gauges like kenwood and agua caliente
+        `Added earlier observation data for USGS gauges like kenwood, agua caliente, and Nathanson
+        `Question for Andy -- how are 0 flows handled in the log normalization? Constant added? Right now plots will just drop them and throw a soft error
         
 
 """
@@ -35,6 +36,8 @@ export_directory=r'Q:\Work_Files\9400_Sonoma\2024UpdateExpandGridOWHM2\UpdateSFR
 File2Process=r'Q:\Work_Files\9400_Sonoma\SVIGFM_Backups\Sept2024_HOBsSFR_testLengthFix\output\SV_sfr_sim.dat' # output filw
 ObservationData=r'Q:\Work_Files\9400_Sonoma\2024UpdateExpandGridOWHM2\UpdateSFR\SFRObservations\CombinedDischargeObservationData.xlsx' # observation data in database format
 Times=r'\\oak-model\public\Work_Files\9400_Sonoma\2024UpdateExpandGridOWHM2\DevelopZonebudget\SP_Date_Lookup_SVIGFM_Hist.xlsx' # SP, Date, and DaysInMonth
+htmlPlots='y' # Y/N binary for plotting html bar plots with all reach-level flows and obs
+CalibPlots='y' # Y/N binary for plotting lines of flow_in and observations
 GaugeLocations={ # dictionary of gauge locations in format 'Name': ['row_col']
 'Sonoma Creek at Agua Caliente Rd': ['100_38'],
 'Sonoma Creek at Verano Ave': ['116_39'],
@@ -83,13 +86,16 @@ OtherStatsAndPlots='n' # I had some code I found useful for looking at outputs. 
 import pandas as pd    
 import re
 #import os
-import plotly.graph_objects as go
+if htmlPlots.lower()=='y' or CalibPlots.lower()=='y':
+    import plotly.graph_objects as go
 import winsound
+import numpy as np
 #import matplotlib
 #import matplotlib.pyplot as plt
 #import numpy as np
 #import time
 #import sys 
+
 
 #==================================DEFINE FUNCTIONS=========================
 def FinishedBeep(): # a function to inform user when script has finished running or completed critical steps (-Patrick Wickham)
@@ -100,7 +106,7 @@ def FinishedBeep(): # a function to inform user when script has finished running
     winsound.Beep(450, 600)
     winsound.Beep(600, 600)
 
-def ProcessSFROutputFile(filepath): # process a file 
+def ProcessSFROutputFile(filepath): # process the sfr output file to read its clunky format
     data = []
     current_period = None
     print("READING: "+filepath)
@@ -113,23 +119,23 @@ def ProcessSFROutputFile(filepath): # process a file
                 current_period = int(period_match.group(1))
                 #print("NEW PERIOD: " + str(current_period))
             
-            # Check if the line is data (contains numbers and follows the format)
+            # Check if the line is data (contains numbers and follows the anticipated format)
             data_match = re.match(r'\s*\d+', line)
             if data_match and current_period is not None:
-                # Split the line into fields (assuming fixed-width format or space-separated values)
+                # Split the line into fields (space-separated values)
                 fields = re.split(r'\s+', line.strip())
                 # Add the current period to the data
                 fields.append(current_period)
                 # Append the row of data to the list
                 data.append(fields)
                 
-    # Adjust the column names to match the data structure (based on your file)    # you may need to chagne this based on what prints from your file
+    # Adjust the column names to match the data s # you may need to change this based on what prints from your file
     column_names = ['LAYER', 'ROW', 'COL', 'SEG', 'RCH', 'FLOW_INTO_RCH', 'FLOW_TO_AQUIFER', 'FLOW_OUT_OF_RCH', 
                     'OVRLND_FLOW', 'DIRECT_PRECIP', 'STREAM_ET', 'STREAM_HEAD', 'STREAM_DEPTH', 'STREAM_WIDTH', 
                     'STREAMBED_COND', 'STREAMBED_GRADIENT', 'STREAMBED_ELEV', 'PERIOD']
     df = pd.DataFrame(data, columns=column_names)
     return df    
-#==================================START WORKING========================= 
+#==================================START PROCESSING DATA========================= 
 # file to dataframe
 df_raw = ProcessSFROutputFile(File2Process)
 df=df_raw.copy(deep=True)
@@ -157,7 +163,7 @@ for column in Columns2Convert:    ReachTimeseries[column] = ReachTimeseries[colu
 
 Observation_df=pd.read_excel(ObservationData)
     
-# print information for By-reach Stats
+#==============================GATHER AND PLOT AT GAUGE LOCATIONS=========================
 print("PRINTING BY REACH")
 CombinedSimulatedGaugeData=pd.DataFrame()
 for gauge_name, row_col in GaugeLocations.items():
@@ -166,30 +172,52 @@ for gauge_name, row_col in GaugeLocations.items():
         clean_gauge_name = gauge_name.replace(" ", "_").replace("/", "_")
         print("PRINTING "+str(gauge_name))
         segment_rchdf=ReachTimeseries[ReachTimeseries['ROW_COL']==UniqueID].copy()
-        fig = go.Figure()
-        fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=-segment_rchdf['FLOW_TO_AQUIFER'],name=str("FLOW_TO_AQUIFER")))   
-        fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=-segment_rchdf['FLOW_OUT_OF_RCH'],name=str("FLOW_OUT_OF_RCH"))) 
-        fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['FLOW_INTO_RCH'],name=str("FLOW_INTO_RCH"))) 
-        fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['OVRLND_FLOW'],name=str("OVRLND_FLOW"))) 
-        fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['DIRECT_PRECIP'],name=str("DIRECT_PRECIP")))  
-        fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['STREAM_ET'],name=str("STREAM_ET")))  
-        
         obs=Observation_df[Observation_df['SiteName']==gauge_name]
-        if len(obs)>0:
-            print("    HAS OBSERVATIONS")
-            fig.add_trace(go.Scatter(x=obs['Month'], y=obs['Discharge'], name="Discharge Observations", mode='markers')) 
         
-        fig.update_layout(title=gauge_name,xaxis_title='Stress Period',yaxis_title='Flows, Model Units (Ft^3/s)',legend_title='flow',barmode='relative')
-        fig.write_html(export_directory+str(clean_gauge_name)+'.html')
-        #fig.write_image(export_directory+str(clean_gauge_name)+'.jpg', scale=5)  # requires kaleido dependency (e.g. conda install kaleido)
+        if htmlPlots.lower()=='y':
+            fig = go.Figure()
+            fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=-segment_rchdf['FLOW_TO_AQUIFER'],name=str("FLOW_TO_AQUIFER")))   
+            fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=-segment_rchdf['FLOW_OUT_OF_RCH'],name=str("FLOW_OUT_OF_RCH"))) 
+            fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['FLOW_INTO_RCH'],name=str("FLOW_INTO_RCH"))) 
+            fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['OVRLND_FLOW'],name=str("OVRLND_FLOW"))) 
+            fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['DIRECT_PRECIP'],name=str("DIRECT_PRECIP")))  
+            fig.add_trace(go.Bar( x=segment_rchdf['Date'], y=segment_rchdf['STREAM_ET'],name=str("STREAM_ET")))  
+        
+            if len(obs)>0:
+                #print("    HAS OBSERVATIONS")
+                fig.add_trace(go.Scatter(x=obs['Month'], y=obs['Discharge'], name="Discharge Observations", mode='markers')) 
+        
+            fig.update_layout(title=gauge_name,xaxis_title='Stress Period',yaxis_title='Flows, Model Units (Ft^3/s)',legend_title='flow',barmode='relative')
+            fig.write_html(export_directory+str(clean_gauge_name)+'.html')
+            #fig.write_image(export_directory+str(clean_gauge_name)+'.jpg', scale=5)
+            #fig.write_image(export_directory+str(clean_gauge_name)+'.jpg', scale=5)  # requires kaleido dependency (e.g. conda install kaleido)
+        if CalibPlots.lower()=='y':
+            fig = go.Figure()   
+            fig.add_trace(go.Scatter( x=segment_rchdf['Date'], y=np.log(segment_rchdf['FLOW_INTO_RCH']),name=str("FLOW_INTO_RCH"),line=dict(color='blue', width=2, dash='solid')))   
+        
+            if len(obs)>0:
+                print("    HAS OBSERVATIONS, PLOTTING CALIBRATION")
+                fig.add_trace(go.Scatter(x=obs['Month'], y=np.log(obs['Discharge']), name="Discharge Observations", mode='markers')) 
+        
+            fig.update_layout(title=gauge_name,xaxis_title='Stress Period',yaxis_title='Flows, Model Units (Ft^3/s), Log Normalized',legend_title='flow',  barmode='relative') #  yaxis_type='log'
+            fig.write_html(export_directory+'Calib_'+str(clean_gauge_name)+'.html')
+            fig.write_image(export_directory+'Calib_'+str(clean_gauge_name)+'.jpg', scale=5) # requires kaleido dependency (e.g. conda install kaleido)
+        
         segment_rchdf.loc[:, 'GaugeName'] = gauge_name
         segment_rchdf.to_excel(export_directory+str(clean_gauge_name)+'.xlsx')
         
         CombinedSimulatedGaugeData = pd.concat([CombinedSimulatedGaugeData, segment_rchdf], ignore_index=True)
 CombinedSimulatedGaugeData=pd.merge(CombinedSimulatedGaugeData,Observation_df,left_on=['GaugeName','Date'],right_on=['SiteName','Month'],how='left')       
 CombinedSimulatedGaugeData['SimulatedLessObserved']=CombinedSimulatedGaugeData['Discharge']-CombinedSimulatedGaugeData['FLOW_INTO_RCH']
+CombinedSimulatedGaugeData['LogObserved']=np.log(CombinedSimulatedGaugeData['Discharge']) 
+CombinedSimulatedGaugeData['LogSimulated']=np.log(CombinedSimulatedGaugeData['FLOW_INTO_RCH']) 
+CombinedSimulatedGaugeData['LogSimulatedLessObserved']=CombinedSimulatedGaugeData['LogObserved']-CombinedSimulatedGaugeData['LogSimulated'] 
+
 CombinedSimulatedGaugeData.to_excel(export_directory+"AllGaugeData.xlsx")    
 
+
+
+#==============================OTHER EXPLORATORY PLOTS IF YOU WANT THEM=========================
 if OtherStatsAndPlots.lower()=='y':
     # print information for model-wide stats
     print("PRINTING MODEL WIDE INFO")
